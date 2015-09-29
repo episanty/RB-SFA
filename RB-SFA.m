1a72a71b926781bc3a4d4450b252b41dba93632b
Emilio Pisanty <e.pisanty11@imperial.ac.uk>
2015-09-29 17:33:38 +0100

    Cleaned up options npp, num and omega

--- RB-SFA.m
+++ RB-SFA.m
@@ -22,7 +22,7 @@
 BeginPackage["RBSFA`"];
 
 
-dipole::usage="dipole[p,\[Kappa]] returns the dipole transition matrix element for a 1s hydrogenic state of ionization potential \!\(\*FractionBox[\(1\), \(2\)]\)\!\(\*SuperscriptBox[\(\[Kappa]\), \(2\)]\).";
+dipole::usage="dipole[p,\[Kappa]] returns the dipole transition matrix element for a 1s hydrogenic state of ionization potential \!\(\*SubscriptBox[\(I\), \(p\)]\)=\!\(\*FractionBox[\(1\), \(2\)]\)\!\(\*SuperscriptBox[\(\[Kappa]\), \(2\)]\).";
 Begin["`Private`"];
 dipole[p_,\[Kappa]_]:=(8I)/\[Pi] (Sqrt[2\[Kappa]^5]p)/(Norm[p]^2+\[Kappa]^2)^3
 End[];
@@ -40,13 +40,13 @@ cosPowerFlatTop[\[Omega]_,num_,power_]:=Function[t,1-Cos[(\[Omega] t)/(2num)]^po
 End[];
 
 
-npp::usage="npp is a sampling option which specifies the number of sampling points per cycle to be used in integrations.";
-num::usage="num is a sampling option which specifies the total number of periods to be integrated over.";
-\[Omega]::usage="\[Omega] is a sampling option which specifies the carrier frequency to be used.";
-Protect[npp,num,\[Omega]];
+PointsPerCycle::usage="PointsPerCycle is a sampling option which specifies the number of sampling points per cycle to be used in integrations.";
+TotalCycles::usage="TotalCycles is a sampling option which specifies the total number of periods to be integrated over.";
+CarrierFrequency::usage="CarrierFrequency is a sampling option which specifies the carrier frequency to be used.";
+Protect[PointsPerCycle,TotalCycles,CarrierFrequency];
 
 
-standardOptions={npp->90,num->1,\[Omega]->0.057};
+standardOptions={PointsPerCycle->90,TotalCycles->1,CarrierFrequency->0.057};
 
 
 harmonicOrderAxis::usage="harmonicOrderAxis[opt\[Rule]value] returns a list of frequencies which can be used as a frequency axis for Fourier transforms, scaled in units of harmonic order, for the provided field duration and sampling options.";
@@ -56,8 +56,7 @@ Protect[LengthCorrection,TargetLength];
 Begin["`Private`"];
 Options[harmonicOrderAxis]=standardOptions~Join~{TargetLength->Automatic,LengthCorrection->1};
 harmonicOrderAxis::target="Invalid TargetLength option `1`. This must be a positive integer or Automatic.";
-harmonicOrderAxis[OptionsPattern[]]:=Module[
-{num=OptionValue[num],npp=OptionValue["npp"]},
+harmonicOrderAxis[OptionsPattern[]]:=Module[{num=OptionValue[TotalCycles],npp=OptionValue[PointsPerCycle]},
 Piecewise[{
 {1/num Range[0.,Round[(npp num+1)/2.]-1+OptionValue[LengthCorrection]],OptionValue[TargetLength]===Automatic},
 {Round[(npp num+1)/2.]/num Range[0,OptionValue[TargetLength]-1]/OptionValue[TargetLength],IntegerQ[OptionValue[TargetLength]]&&OptionValue[TargetLength]>=0}
@@ -71,7 +70,7 @@ End[];
 frequencyAxis::usage="frequencyAxis[opt\[Rule]value] returns a list of frequencies which can be used as a frequency axis for Fourier transforms, in atomic units of frequency, for the provided field duration and sampling options.";
 Begin["`Private`"];
 Options[frequencyAxis]=Options[harmonicOrderAxis];
-frequencyAxis[options:OptionsPattern[]]:=OptionValue[\[Omega]]harmonicOrderAxis[options]
+frequencyAxis[options:OptionsPattern[]]:=OptionValue[CarrierFrequency]harmonicOrderAxis[options]
 End[];
 
 
@@ -83,14 +82,14 @@ Protect[TimeScale,AtomicUnits,LaserPeriods];
 Begin["`Private`"];
 Options[timeAxis]=standardOptions~Join~{TimeScale->AtomicUnits};
 timeAxis::scale="Invalid TimeScale option `1`. Available values are AtomicUnits and LaserPeriods";
-timeAxis[OptionsPattern[]]:=Module[{T=2\[Pi]/OptionValue[\[Omega]],num=OptionValue[num]},
+timeAxis[OptionsPattern[]]:=Block[{T=2\[Pi]/\[Omega],\[Omega]=OptionValue[CarrierFrequency],num=OptionValue[TotalCycles],npp=OptionValue[PointsPerCycle]},
 Piecewise[{
 {1,OptionValue[TimeScale]==AtomicUnits},
 {1/T,OptionValue[TimeScale]==LaserPeriods}
 },
 Message[timeAxis::scale,OptionValue[TimeScale]];Abort[]
 ]*Table[t
-,{t,0,num (2\[Pi])/OptionValue[\[Omega]],num/(num*OptionValue[npp]+1) (2\[Pi])/OptionValue[\[Omega]]}
+,{t,0,num (2\[Pi])/\[Omega],num/(num*npp+1) (2\[Pi])/\[Omega]}
 ]
 ]
 End[];
@@ -109,26 +108,25 @@ Options[getSpectrum]={Polarization->False,ComplexPart->(#&),\[Omega]Power->0,Dif
 getSpectrum::diffOrd="Invalid differentiation order `1`.";
 getSpectrum::\[Omega]Pow="Invalid \[Omega] power `1`.";
 
-getSpectrum[dipoleList_,OptionsPattern[]]:=With[{names=Options[getSpectrum][[All,1]]},
-Block[names,names=OptionValue[names];Block[
+getSpectrum[dipoleList_,OptionsPattern[]]:=Block[
 {polarizationVector,differentiatedList,depth,dimensions,
-\[Delta]t=(2\[Pi]/\[Omega])/npp
+num=OptionValue[TotalCycles],npp=OptionValue[PointsPerCycle],\[Omega]=OptionValue[CarrierFrequency],\[Delta]t=(2\[Pi]/\[Omega])/npp
 },
-polarizationVector=Polarization/Norm[Polarization];
+polarizationVector=OptionValue[Polarization]/Norm[OptionValue[Polarization]];
 
-differentiatedList=ComplexPart[Piecewise[{
-{dipoleList,DifferentiationOrder==0},
-{1/(2\[Delta]t) (Most[Most[dipoleList]]-Rest[Rest[dipoleList]]),DifferentiationOrder==1},
-{1/\[Delta]t^2 (Most[Most[dipoleList]]-2Most[Rest[dipoleList]]+Rest[Rest[dipoleList]]),DifferentiationOrder==2}},
-Message[getSpectrum::diffOrd,DifferentiationOrder];Abort[]
+differentiatedList=OptionValue[ComplexPart][Piecewise[{
+{dipoleList,OptionValue[DifferentiationOrder]==0},
+{1/(2\[Delta]t) (Most[Most[dipoleList]]-Rest[Rest[dipoleList]]),OptionValue[DifferentiationOrder]==1},
+{1/\[Delta]t^2 (Most[Most[dipoleList]]-2Most[Rest[dipoleList]]+Rest[Rest[dipoleList]]),OptionValue[DifferentiationOrder]==2}},
+Message[getSpectrum::diffOrd,OptionValue[DifferentiationOrder]];Abort[]
 ]];
 
-If[NumberQ[\[Omega]Power],Null;,Message[getSpectrum::\[Omega]Pow,\[Omega]Power];Abort[]  ];
+If[NumberQ[OptionValue[\[Omega]Power]],Null;,Message[getSpectrum::\[Omega]Pow,OptionValue[\[Omega]Power]];Abort[]  ];
 
 num Table[
-(\[Omega]/num k)^(2\[Omega]Power),{k,1,Round[Length[differentiatedList]/2]}
+(\[Omega]/num k)^(2OptionValue[\[Omega]Power]),{k,1,Round[Length[differentiatedList]/2]}
 ]*If[
-Polarization===False,(*unpolarized spectrum*)
+OptionValue[Polarization]===False,(*unpolarized spectrum*)
 (*funky depth thing so this can take lists of numbers and lists of vectors, of arbitrary length. Makes for easier benchmarking.*)
 depth=Length[Dimensions[dipoleList]];
 dimensions=If[Length[#]>1,#[[2]],1(*#\[LeftDoubleBracket]1\[RightDoubleBracket]*)]&[Dimensions[dipoleList]];
@@ -148,7 +146,7 @@ Re[differentiatedList[[All,i]]]
 ,{i,1,2}]][[1;;Round[Length[differentiatedList]/2]]].polarizationVector
 ]^2
 ]
-]]]
+]
 End[];
 
 
@@ -210,14 +208,14 @@ VectorPotentialGradient::usage="VectorPotentialGradient is an option for makeDip
 FieldParameters::usage="FieldParameters is an option for makeDipole list which ";
 Preintegrals::usage="Preintegrals is an option for makeDipole list which specifies whether the preintegrals of the vector potential should be \"Analytic\" or \"Numeric\".";
 ReportingFunction::usage="ReportingFunction is an option for makeDipole list which specifies a function used to report the results, either internally (by the default, Identity) or to an external file.";
-Gate::usage="Gate is an option for makeDipole list which specifies the integration gate to use. Usage as Gate\[Rule]g, nGate\[Rule]n will gate the integral at time t/\[Omega] by g[t,n]. The default is Gate\[Rule]SineSquaredGate[1/2].";
+Gate::usage="Gate is an option for makeDipole list which specifies the integration gate to use. Usage as Gate\[Rule]g, nGate\[Rule]n will gate the integral at time \[Omega]t/\[Omega] by g[\[Omega]t,n]. The default is Gate\[Rule]SineSquaredGate[1/2].";
 nGate::usage="nGate is an option for makeDipole list which specifies the total number of cycles in the integration gate.";
-\[Kappa]::usage="\[Kappa] is an option for makeDipole list which specifies the ionization potential \!\(\*SubscriptBox[\(I\), \(p\)]\)=\!\(\*FractionBox[\(1\), \(2\)]\)\!\(\*SuperscriptBox[\(\[Kappa]\), \(2\)]\) of the target.";
+IonizationPotential::usage="IonizationPotential is an option for makeDipole list which specifies the ionization potential \!\(\*SubscriptBox[\(I\), \(p\)]\) of the target.";
 \[Epsilon]Correction::usage="\[Epsilon]Correction is an option for makeDipole list which specifies the regularization correction \[Epsilon], i.e. as used in the factor \!\(\*FractionBox[\(1\), SuperscriptBox[\((t - tt + \[ImaginaryI]\\\ \[Epsilon])\), \(3/2\)]]\).";
 PointNumberCorrection::usage="PointNumberCorrection is an option for makeDipole list which specifies an extra number of points to be integrated over, which is useful to prevent Indeterminate errors when a Piecewise envelope is being differentiated at the boundaries.";
 
 
-Protect[VectorPotential,VectorPotentialGradient,FieldParameters,Preintegrals,ReportingFunction,Gate,\[Kappa],nGate,nGateRamp,\[Epsilon]Correction];
+Protect[VectorPotential,VectorPotentialGradient,FieldParameters,Preintegrals,ReportingFunction,Gate,IonizationPotential,nGate,nGateRamp,\[Epsilon]Correction];
 
 
 Begin["`Private`"];
@@ -225,7 +223,7 @@ Options[makeDipoleList]=standardOptions~Join~{
 VectorPotential->Automatic,FieldParameters->{},VectorPotentialGradient->None,
 Preintegrals->"Analytic",ReportingFunction->Identity,
 Gate->SineSquaredGate[1/2],nGate->3/2,
-\[Epsilon]Correction->0.1,\[Kappa]->1.,
+\[Epsilon]Correction->0.1,IonizationPotential->0.5,
 PointNumberCorrection->0,Verbose->0
 };
 makeDipoleList::gate="The integration gate g provided as Gate\[Rule]`1` is incorrect. Its usage as g[`2`,`3`] returns `4` and should return a number.";
@@ -235,63 +233,62 @@ makeDipoleList::preint="Wrong Preintegrals option `1`. Valid options are \"Analy
 
 
 
-makeDipoleList[OptionsPattern[]]:=With[{names=Options[makeDipoleList][[All,1]]},
-Block[names,names=OptionValue[names];
-Block[
+makeDipoleList[OptionsPattern[]]:=Block[
 {
+num=OptionValue[TotalCycles],npp=OptionValue[PointsPerCycle],\[Omega]=OptionValue[CarrierFrequency],\[Kappa]=Sqrt[2OptionValue[IonizationPotential]],
 A,F,GA,pi,ps,S,
 gate,tGate,setPreintegral,
-tol,gridPointQ,tInit,tFinal,\[Delta]t,\[Epsilon]=\[Epsilon]Correction,
+tol,gridPointQ,tInit,tFinal,\[Delta]t,\[Epsilon]=OptionValue[\[Epsilon]Correction],
 AInt,A2Int,GAInt,GAdotAInt,AdotGAInt,GAIntInt,bigPScorrectionInt,AdotGAdotAInt,
 AIntList,A2IntList,GAIntList,GAdotAIntList,AdotGAIntList,GAIntIntList,bigPScorrectionIntList,AdotGAdotAIntList,
 dipoleList
 },
 
-A[t_]=VectorPotential[t]//.FieldParameters;
+A[t_]=OptionValue[VectorPotential][t]//.OptionValue[FieldParameters];
 F[t_]=-D[A[t],t];
 GA[t_]=If[
-TrueQ[VectorPotentialGradient==None],        Table[0,{Length[A[tInit]]},{Length[A[tInit]]}],
-VectorPotentialGradient[t]//.FieldParameters
+TrueQ[OptionValue[VectorPotentialGradient]==None],        Table[0,{Length[A[tInit]]},{Length[A[tInit]]}],
+OptionValue[VectorPotentialGradient][t]//.OptionValue[FieldParameters]
 ];
 
 tInit=0;
 tFinal=(2\[Pi])/\[Omega] num;
-\[Delta]t=(tFinal-tInit)/(num*npp+PointNumberCorrection);(*integration and looping timestep*)
-tGate=nGate (2\[Pi])/\[Omega];
+\[Delta]t=(tFinal-tInit)/(num*npp+OptionValue[PointNumberCorrection]);(*integration and looping timestep*)
+tGate=OptionValue[nGate] (2\[Pi])/\[Omega];
 (*Check potential and potential gradient for correctness.*)
 With[{\[Omega]tRandom=RandomReal[{\[Omega] tInit,\[Omega] tFinal}]},
-If[!And@@(NumberQ/@A[\[Omega]tRandom/\[Omega]]),Message[makeDipoleList::pot,VectorPotential,\[Omega]tRandom,A[\[Omega]tRandom]];Abort[]];
-If[!And@@(NumberQ/@Flatten[GA[\[Omega]tRandom/\[Omega]]]),Message[makeDipoleList::gradpot,VectorPotentialGradient,\[Omega]tRandom,GA[\[Omega]tRandom]];Abort[]];
+If[!And@@(NumberQ/@A[\[Omega]tRandom/\[Omega]]),Message[makeDipoleList::pot,OptionValue[VectorPotential],\[Omega]tRandom,A[\[Omega]tRandom]];Abort[]];
+If[!And@@(NumberQ/@Flatten[GA[\[Omega]tRandom/\[Omega]]]),Message[makeDipoleList::gradpot,OptionValue[VectorPotentialGradient],\[Omega]tRandom,GA[\[Omega]tRandom]];Abort[]];
 ];
 
 Which[
-Preintegrals=="Analytic",
+OptionValue[Preintegrals]=="Analytic",
 gridPointQ[_]=True;
-,Preintegrals=="Numeric",
+,OptionValue[Preintegrals]=="Numeric",
 tol=10^-5;gridPointQ[t_]:=gridPointQ[t]=Abs[(t-tInit)/\[Delta]t-Round[(t-tInit)/\[Delta]t]]<tol&&tInit-tol<=t<=tFinal+tol;
 (*Checks whether the given time is part of the time grid in use, up to tolerance tol.*)
-,True,Message[makeDipoleList::preint,Preintegrals];Abort[];
+,True,Message[makeDipoleList::preint,OptionValue[Preintegrals]];Abort[];
 ];
 
-gate[\[Omega]\[Tau]_]:=Gate[\[Omega]\[Tau],nGate];
+gate[\[Omega]\[Tau]_]:=OptionValue[Gate][\[Omega]\[Tau],OptionValue[nGate]];
 With[{\[Omega]tRandom=RandomReal[{\[Omega] tInit,\[Omega] tFinal}]},
 If[!TrueQ[NumberQ[gate[\[Omega]tRandom]]],
-Message[makeDipoleList::gate,Gate,\[Omega]tRandom,nGate,gate[\[Omega]tRandom]];Abort[]]
+Message[makeDipoleList::gate,OptionValue[Gate],\[Omega]tRandom,OptionValue[nGate],gate[\[Omega]tRandom]];Abort[]]
 ];
 
 
 setPreintegral[integralVariable_,listVariable_,preintegrand_,nullValue_:False]:=Which[
-VectorPotentialGradient=!=None||nullValue===False,(*Vector potential gradient specified, or integral variable does not depend on it, so integrate*)
+OptionValue[VectorPotentialGradient]=!=None||nullValue===False,(*Vector potential gradient specified, or integral variable does not depend on it, so integrate*)
 Which[
-Preintegrals=="Analytic",
+OptionValue[Preintegrals]=="Analytic",
 integralVariable[t_]=Integrate[preintegrand[t],t];
 integralVariable[t_,tt_]=((#/.{\[Tau]->t})-(#/.{\[Tau]->tt}))&[Integrate[preintegrand[\[Tau]],\[Tau]]];
-,Preintegrals=="Numeric",
+,OptionValue[Preintegrals]=="Numeric",
 listVariable=\[Delta]t*Accumulate[Table[preintegrand[t],{t,tInit,tFinal,\[Delta]t}]];
 integralVariable[t_?gridPointQ,tt_?gridPointQ]:=listVariable[[Round[t/\[Delta]t+1]]]-listVariable[[Round[tt/\[Delta]t+1]]];
 integralVariable[t_?gridPointQ]:=integralVariable[t,tInit];
 ];
-,VectorPotentialGradient===None,(*No vector potential has been specified, return appropriate zero matrix*)
+,OptionValue[VectorPotentialGradient]===None,(*No vector potential has been specified, return appropriate zero matrix*)
 integralVariable[t_]=nullValue;
 integralVariable[t_,tt_]=nullValue;
 ];
@@ -346,22 +343,22 @@ ps[t,tt].GAIntInt[t,tt].ps[t,tt]+ps[t,tt].bigPScorrectionInt[t,tt]+AdotGAdotAInt
 
 (*Debugging constructs. Verbose\[Rule]1 prints information about the internal functions. Verbose\[Rule]2 returns all the relevant internal functions and stops.*)
 Which[
-Verbose==1,Information/@{ps,pi,S,AInt,A2Int,GAInt,GAdotAInt,AdotGAInt,GAIntInt,bigPScorrectionInt,AdotGAdotAInt},
-Verbose==2,Return[With[{t=Global`t,tt=Global`tt,p=Global`t,\[Tau]=Global`\[Tau]},
+OptionValue[Verbose]==1,Information/@{ps,pi,S,AInt,A2Int,GAInt,GAdotAInt,AdotGAInt,GAIntInt,bigPScorrectionInt,AdotGAdotAInt},
+OptionValue[Verbose]==2,Return[With[{t=Global`t,tt=Global`tt,p=Global`t,\[Tau]=Global`\[Tau]},
 {ps[t,tt],pi[p,t],S[t,tt],AInt[t],AInt[t,tt],A2Int[t],A2Int[t,tt],GAInt[t],GAInt[t,tt],GAdotAInt[t],GAdotAInt[t,tt],AdotGAInt[t],AdotGAInt[t,tt],GAIntInt[t],GAIntInt[t,tt],bigPScorrectionInt[t],bigPScorrectionInt[t,tt],AdotGAdotAInt[t],AdotGAdotAInt[t,tt],I ((2\[Pi])/(\[Epsilon]+I \[Tau]))^(3/2) dipole[pi[ps[t,t-\[Tau]],t],\[Kappa]]\[Conjugate]*dipole[pi[ps[t,t-\[Tau]],t-\[Tau]],\[Kappa]].F[t-\[Tau]]Exp[-I S[t,t-\[Tau]]]gate[\[Omega] \[Tau]]}]]
 ];
 
 (*Numerical integration loop*)
 (dipoleList=Table[
-ReportingFunction[
+OptionValue[ReportingFunction][
 \[Delta]t Sum[(
 I ((2\[Pi])/(\[Epsilon]+I \[Tau]))^(3/2) dipole[pi[ps[t,t-\[Tau]],t],\[Kappa]]\[Conjugate]*dipole[pi[ps[t,t-\[Tau]],t-\[Tau]],\[Kappa]].F[t-\[Tau]]Exp[-I S[t,t-\[Tau]]]gate[\[Omega] \[Tau]] 
-),{\[Tau],0,If[Preintegrals=="Analytic",tGate,Min[t-tInit,tGate]],\[Delta]t}]
+),{\[Tau],0,If[OptionValue[Preintegrals]=="Analytic",tGate,Min[t-tInit,tGate]],\[Delta]t}]
 ]
 ,{t,tInit,tFinal,\[Delta]t}]);
 dipoleList
 
-]]]
+]
 End[];
 
 
