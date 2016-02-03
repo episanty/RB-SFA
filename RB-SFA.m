8e2cae45159e2159a6d06d4162e07664a89597cc
Emilio Pisanty <e.pisanty11@imperial.ac.uk>
2016-02-03 22:06:06 +0000

    Diagnostics on the integration errors - they appear harmless.
    They happen at taupre close to tInit when t and tt are nowhere near. I'm 
    cutting down the (overbroad) integration range for that case.

--- RB-SFA.m
+++ RB-SFA.m
@@ -24,7 +24,7 @@ BeginPackage["RBSFA`"];
 
 RBSFAversion::usage="RBSFAversion[] prints the current version of the RB-SFA package in use and its timestamp.";
 Begin["`Private`"];
-RBSFAversion[]="RB-SFA v2.0.1, Wed 3 Feb 2016 21:50:43";
+RBSFAversion[]="RB-SFA v2.0.1, Wed 3 Feb 2016 22:04:30";
 End[];
 
 
@@ -347,6 +347,7 @@ Array[innervariable[##][tt]==0&,dimensions]
 }]
 ,Flatten[Array[innervariable[##]&,dimensions]]
 ,{\[Tau]pre,tInit,tFinal}
+,EvaluationMonitor:>Sow[\[Tau]pre]
 ]
 ,Print[{integralVariable,t,tt}];result
 ]];
@@ -402,7 +403,33 @@ SubscriptBox[\(t\), \(0\)], \(t\)]A\((\[Tau])\)\[CenterDot]\[Del]A\((\[Tau])\)\[
 Print[AdotGAdotAInt[15,1]];
 Print[bigPScorrectionInt[15,1]];*)
 
-Print[bigPScorrectionInt[140.85113261708622`,137.1767552444666`]];
+(*Print[bigPScorrectionInt[140.85113261708622`,137.1767552444666`]];
+Print[bigPScorrectionInt[140.85113261708622`,137.17675524446`]];*)
+
+aPrint[
+list=Reap[
+bigPScorrectionInt[140.85113261708622`,137.1767552444666`]
+][[2,1]]
+];
+
+Print[
+Block[{tt=137.17675524446`,j=1},
+ListPointPlot3D[
+{j++,#,(GAdotAInt[#,tt]+A[#].GAInt[#,tt])[[3]]}&/@list
+,ImageSize->900
+,AxesLabel->{"j","t","integrand"}
+]
+]
+];
+
+(*Print[
+Block[{tt=137.17675524446`},
+Plot[
+(GAdotAInt[t,tt]+A[t].GAInt[t,tt])\[LeftDoubleBracket]3\[RightDoubleBracket]
+,{t,0,150}
+]
+]
+];*)
 
 Return[];
 
