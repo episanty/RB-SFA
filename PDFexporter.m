bfbd706d37d206cc607ab065166cad697facb937
Emilio Pisanty <pisanty@mbi-berlin.de>
2016-05-02 16:34:13 +0200

    Added PDFexporter to repo
    including better handling of directory to remove hard-coded location.

---
+++ PDFexporter.m
@@ -0,0 +1,32 @@
+(* ::Package:: *)
+
+(*
+This file is for internal handling and is used to create
+pdf printouts of the main code notebook RB-SFA.nb and the 
+Usage and Examples.nb notebook.
+
+It can be run directly from the terminal with the command
+   MathKernel -script PDFexporter.m
+or it can be run directly from Mathematica.
+*)
+
+
+directory=Quiet[Check[NotebookDirectory[],DirectoryName[$InputFileName]],{FrontEndObject::notavail}];
+
+
+Export[
+FileNameJoin[{directory,"RB-SFA.pdf"}],
+Import[FileNameJoin[{directory,"RB-SFA.nb"}]]/.{Closed->Open}
+];
+
+
+Export[
+FileNameJoin[{directory,"Usage and Examples.pdf"}],
+Import[FileNameJoin[{directory,"Usage and Examples.nb"}]]/.{
+Cell[CellGroupData[{
+Cell[contents__,CellTags->"Collapse-to-print"],otherCells___
+},Closed]]->Cell[CellGroupData[{
+Cell[contents,CellTags->"Collapse-to-print"],otherCells
+},TempClosed]]
+}/.{Closed->Open,TempClosed->Closed}
+];
